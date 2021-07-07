2a767f0f90f072a21f441c66e27629fa464cced0
chzar <cameron.harrell@bouvet.no>
2021-07-07 15:27:40 +0200

    slimmed comment

--- internal/plugin/constants.go
+++ internal/plugin/constants.go
@@ -6,11 +6,11 @@ const (
 	storageAccountKeyEnvVarConfigKey = "storageAccountKeyEnvVar"
 	subscriptionIDConfigKey          = "subscriptionId"
 	blockSizeConfigKey               = "blockSizeInBytes"
+	credentialsFileEnvVar            = "AZURE_CREDENTIALS_FILE"
 	encryptionKeyEnvVar              = "AZURE_ENCRYPTION_KEY"
 	encryptionHashEnvVar             = "AZURE_ENCRYPTION_HASH"
 
-	// blocks must be less than/equal to 100MB in size
-	// ref. https://docs.microsoft.com/en-us/rest/api/storageservices/put-block#uri-parameters
+	// see https://docs.microsoft.com/en-us/rest/api/storageservices/put-block#uri-parameters
 	defaultBlockSize = 100 * 1024 * 1024
 	blob_url_suffix  = "https://%s.blob.core.windows.net"
 )
