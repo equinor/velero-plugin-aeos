dcd6f5ee0506a8d01ee4ded74f6bdca1ef36d70d
chzar <cameron.harrell@bouvet.no>
2021-07-07 20:47:32 +0200

    added funcs for loading of secret file

--- internal/plugin/helpers.go
+++ internal/plugin/helpers.go
@@ -1,1 +1,70 @@
 package plugin
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func retrieveRequiredSecrets(secretNames []string) (map[string]string, error) {
+	var secretsMap = make(map[string]string)
+
+	for _, secretName := range secretNames {
+		envVar, found := os.LookupEnv(secretName)
+		if !found {
+			return secretsMap, fmt.Errorf("required env var %s not set", secretName)
+		}
+		secretsMap[secretName] = envVar
+	}
+
+	return secretsMap, nil
+}
+
+func loadSecretsFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return errors.New("failed to open")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		parseLineToEnv(scanner.Text())
+	}
+	return nil
+}
+
+func parseLineToEnv(text string) error {
+	cleanedText := strings.TrimSpace(text)
+	inputs := strings.Split(cleanedText, "=")
+
+	if len(inputs) != 2 {
+		return errors.New("failed parse as valid env var assignment")
+	}
+
+	if isValidEnvVarName(inputs[0]) {
+		os.Setenv(inputs[0], inputs[1])
+	}
+	return nil
+}
+
+func isValidEnvVarName(text string) bool {
+	var output string = ""
+
+	for _, x := range text {
+		if unicode.IsUpper(x) {
+			if unicode.IsLetter(x) || x == rune('_') {
+				output = output + string(x)
+			}
+		}
+	}
+
+	if text == output {
+		return true
+	}
+	return false
+}
