045368d6678cdc1ff890f3b7e27b6765a971e61e
chzar <cameron.harrell@bouvet.no>
2021-07-08 22:12:24 +0200

    renamed plugin

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 
 func main() {
 	framework.NewServer().
-		RegisterObjectStore("equinor/azure-encrypted-object-store", newObjectStorePlugin).
+		RegisterObjectStore("equinor/velero-plugin-aeos", newObjectStorePlugin).
 		Serve()
 }
 
