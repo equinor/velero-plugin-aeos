ca568433fd59c2fb014c6018e61bb7ec6149c53f
chzar <camjharrell@gmail.com>
2021-12-22 16:53:00 +0100

    pass cmdflags to plugin (#11)
    * pass cmdflags to plugin
    
    Co-authored-by: chzar <cameron.harrell@bouvet.no>

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/equinor/velero-plugin-aeos/internal/plugin"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	"github.com/vmware-tanzu/velero/pkg/plugin/framework"
 )
 
 func main() {
 	framework.NewServer().
+		BindFlags(pflag.CommandLine).
 		RegisterObjectStore("equinor/velero-plugin-aeos", newObjectStorePlugin).
 		Serve()
 }
